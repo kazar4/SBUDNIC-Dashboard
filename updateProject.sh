5425ae486a2d6124310a5df8c37fa1e91b63a5fd
Kazen Gallman <kazar444@icloud.com>
2022-04-01 19:51:53 -0400

    changing server script

--- updateProject.sh
+++ updateProject.sh
@@ -4,14 +4,18 @@ echo Run in SBUDNIC-Dashboard directory
 echo
 echo
 git pull
+cd client
 npm install
 npm run build
+cd ../
 cp -r client/public/sbudnic/ ../
-cd sever/
+cd server/
 rm -r ssdv/
 rm -r ssdv
+mkdir ssdvNEW
+cd ssdvNEW
 git clone https://github.com/fsphil/ssdv.git
 cd ssdv
 make
-cp ssdv ../
+cp ssdv ../../
 echo run server with python3 server.py --prod on a seperate screen
